75fa08d8e0009bce4e9c323826602c77426a3643
rolysr <65413231+rolysr@users.noreply.github.com>
2023-04-04 12:33:41 -0400

    Revert "Add GPT4All quatized model"
    This reverts commit 8da6ada0a82e849aadf2db7ad09d621f3052a88b.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from graph_contractor.graph_contractor import GraphContractor
+from gpt4all.generate import generate
 
 
-gc = GraphContractor(url='neo4j+s://5fc1bc71.databases.neo4j.io',password='GUIPdTc9tUjzTw63n47eFbcc6_rFbhjyI_OAg0tbcsU')
-print(gc.entities)
+# gc = GraphContractor(url='neo4j+s://5fc1bc71.databases.neo4j.io',password='GUIPdTc9tUjzTw63n47eFbcc6_rFbhjyI_OAg0tbcsU')
+# print(gc.entities)
