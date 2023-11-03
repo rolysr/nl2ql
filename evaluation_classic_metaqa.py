84cacf54c430f35c880b66772a93fcdd1d3c372e
rolysr <rolsanchez@yandex.com>
2023-11-02 20:07:45 -0400

    Update evaluation_classic_metaqa.py

--- evaluation_classic_metaqa.py
+++ evaluation_classic_metaqa.py
@@ -4,9 +4,11 @@ from generation.metaqa import generate_metaqa_tests
 from metaqakb import MetaQAKnowledgeBase
 from model import get_model
 import time
+from langchain.callbacks import get_openai_callback
 
 import os
 from dotenv import load_dotenv
+from query_utils import QueryUtils
 from schema import DBSchemaMaker
 from utils import put_results_on_files, update_metrics_tests
 import logging
@@ -87,7 +89,41 @@ for i in range(len(tests)):
 
     # update metrics for each test case
     start_time = time.time()
-    experiment_total_cost = update_metrics_tests(model=model, tests=hop_tests, query_language=query_language, database_type=database_type, schema=schema, gc=gc, successful_compilations=successful_compilations, unsuccessful_compilations=unsuccessful_compilations, correct_responses=correct_responses, wrong_responses=wrong_responses, logger=logger)
+    test_index = 1
+    experiment_total_cost = 0
+    for test in hop_tests:
+        logger.info("Checking test: {}".format(test_index))
+        test_index += 1
+
+        # get natural language query and target formal query
+        query, target_response = test
+
+        # make the natural language query to the model
+        with get_openai_callback() as cb:
+            formal_query = model.run(query_language=query_language,
+                                     database_type=database_type, schema=schema, query=query)
+
+        # update total cost
+        experiment_total_cost += cb.total_cost
+
+        # run the formal query in the database
+        try:
+            response = gc.make_query(formal_query)
+            new_test = (test[0], test[1], formal_query)
+            successful_compilations.append(new_test)
+        except:  # in case of no compilation, continue
+            new_test = (test[0], test[1], formal_query)
+            unsuccessful_compilations.append(new_test)
+            wrong_responses.append(new_test)
+            continue
+
+        # check responses match
+        if QueryUtils.equal_classic_query_results(response, target_response):
+            new_test = (test[0], test[1], formal_query)
+            correct_responses.append(new_test)
+        else:
+            new_test = (test[0], test[1], formal_query)
+            wrong_responses.append(new_test)
     end_time = time.time() - start_time
     
     # print results
